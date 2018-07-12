475784c7b916bcc9906b84edeb89132e7d9aa794
IzumiSakai1995 <IzumiSakai1995@gmail.com>
2018-07-12 20:22:53 +0800

    update

---
+++ 流程控制/for_.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/12 19:28:21
+# @Author  : Izumi Sakai
+# @File    : for_.py
+# @Software: PyCharm
+#python中语句依据任意序列中的子项，按它们在序列中的顺序来进行迭代
---
+++ 流程控制/if_.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/12 19:22:51
+# @Author  : Izumi Sakai
+# @File    : if_.py
+# @Software: PyCharm
+
+# x = int(input('please input a integer:'))
+# # if x<0:
+# #     pass
+# # elif:
+# #     pass
+# # elif:
+# #     pass
+# # else:
+# #     pass
+# 零到多个elif部分。
