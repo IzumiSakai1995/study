dcffac6089d1461dc8c5da5c8d66cc03f975a7aa
IzumSakai1995 <IzumiSakai@gmail1995.com>
2018-07-16 01:28:29 +0800

    update

---
+++ 模块和包/random_.py
@@ -0,0 +1,20 @@
+# author: Liu Zheng
+# date:2018/7/16
+
+import random
+# print(random.random())
+# print(random.randint(1,8))  # 包括8
+# print(random.choice('hello'))   # 内部放一个序列
+# print(random.shuffle)
+# print(random.sample(['123','1','3'],2))
+# print(random.randrange(1,3))    #只有1~2
+
+def v_code():
+    code = ''
+
+    pass
+
+
+
+
+v_code()
--- 模块和包/time_.py
+++ 模块和包/time_.py
@@ -3,8 +3,23 @@
 # @Author  : Izumi Sakai
 # @File    : time_.py
 # @Software: PyCharm
-
+import datetime
 import time  # provides various functions to manipulate time values
-print(help(time))
-# print(time.time())   输出时间戳
-# time.sleep(secs)
+# print(help(time))
+# print(time.time())   输出时间戳  ****
+# time.sleep(secs)      ******
+# print(time.clock())  # 计算cpu执行时间
+# print(time.gmtime())  # UTC 通用协调时  英国时间 time.struct_time(tm_year=2018, tm_mon=7, tm_mday=15, tm_hour=16,
+# tm_min=26, tm_sec=48, tm_wday=6, tm_yday=196, tm_isdst=0 夏令时时间)
+# print((time.localtime())) *****
+# print(time.strftime('%Y %m %d %H %M %S')) *****  字符串时间
+# print(time.strptime(time.strftime()))  转格式化时间
+# a = time.strptime()
+# a.tm_year
+# print(time.ctime()) 格式定义好
+# print(time.ctime(3600))
+# print(time.mktime(time.localtime()))  #参数为tuple  结构化时间
+
+
+# print(datetime.datetime.now())
+print(datetime.datetime)
