8ed0b5cea3043da734fc9ad9fae1260e7b3612e4
IzumiSakai1995 <IzumiSakai1995@gmail.com>
2018-07-15 23:29:32 +0800

    update

---
+++ 模块和包/time_.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/15 23:25:36
+# @Author  : Izumi Sakai
+# @File    : time_.py
+# @Software: PyCharm
+
+import time  # provides various functions to manipulate time values
+print(help(time))
+# print(time.time())   输出时间戳
+# time.sleep(secs)
