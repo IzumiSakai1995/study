820e6d75b2f633c60801ee8ce29a6414b5f392fe
IzumiSakai1995 <IzumiSakai1995@gmail.com>
2018-07-17 17:34:11 +0800

    update

---
+++ 目录结构/dir_.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/17 01:20:56
+# @Author  : Izumi Sakai
+# @File    : dir_.py
+# @Software: PyCharm
+
+# project
+#     bin/
+#         foo
+#
+#     foo
+#         test
+#             __init__.py
+#             test.main.py
+#
+#         __init__.py
+#         main.py
+#
+#     docs
+#         conf.py
+#         abc.rst
+#
+#     setup.py
+#                 --> 代码的打包安装部署。
+#     requirement.txt
+#     README-----
+#                - 软件定位，软件基本功能
+#                -运行代码方法:安装环境、启动命令
+#                -简要使用说明
+#                -代码目录结构说明，更详细的可以说明软件的基本原理
+#                -常见问题说明  有关项目的基本介绍
+
